0ef4edbbe88890b5e3e5edeae060a908da6ced0c
ferengitooth <ehitsamill@gmail.com>
2021-08-31 19:07:28 -0700

    hooked up get find all

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,10 +1,18 @@
 // implement your posts router here
 const express = require('express')
-
+const Post = require('./posts-model')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The post with the specified ID does not exist"
+        })
+    })
 })
 router.get('/:id', (req, res) => {
 
